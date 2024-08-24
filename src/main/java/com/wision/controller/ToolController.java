9d84dbcd88e99280df1e9f64aac12a5cc85e6ce4
kacrote1987 <751613339@qq.com>
2024-08-24 23:48:07 +0800

    Update README.md

---
+++ src/main/java/com/wision/controller/ToolController.java
@@ -0,0 +1,54 @@
+package com.wision.controller;
+
+import com.github.pagehelper.PageInfo;
+import com.wision.entity.ToolDetForm;
+import com.wision.entity.ToolDetVo;
+import com.wision.entity.ToolListForm;
+import com.wision.entity.ToolListVo;
+import com.wision.service.ToolService;
+import com.wision.util.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Api(description = "应用支撑体系")
+@RestController
+@RequestMapping("/tool")
+public class ToolController {
+    @Resource
+    ToolService toolService;
+
+    @ApiOperation("组件列表")
+    @PostMapping("/toollist")
+    public Result toolList(ToolListForm params){
+        PageInfo<ToolListVo> toolList= toolService.toolList(params);
+        return Result.success(toolList);
+    }
+
+    @ApiOperation("组件详细")
+    @PostMapping("/tooldet")
+    public Result toolDet(@RequestBody String params){
+        List<ToolDetVo> toolDet= toolService.toolDet(params);
+        return Result.success(toolDet);
+    }
+
+    @ApiOperation("组件保存")
+    @PostMapping("/toolsave")
+    public Result toolSave(@RequestBody ToolDetForm params){
+        toolService.toolSave(params);
+        return Result.success();
+    }
+
+    @ApiOperation("组件查询")
+    @PostMapping("/toolsel")
+    public Result toolSel(@RequestBody String prodId,ToolListForm params){
+        PageInfo<ToolListVo> toolSel= toolService.toolSel(prodId,params);
+        return Result.success(toolSel);
+    }
+}
