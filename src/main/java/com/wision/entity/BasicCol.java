871b8c012ce069a6217b1ad96966043d042dc329
kacrote1987 <751613339@qq.com>
2024-09-01 21:50:59 +0800

    Update README.md

---
+++ src/main/java/com/wision/entity/BasicCol.java
@@ -0,0 +1,58 @@
+package com.wision.entity;
+
+public class BasicCol {
+    private Long extId;
+    private String toolCode;
+    private String toolName;
+    private String toolType;
+    private String resState;
+    private String selState;
+
+    public Long getExtId() {
+        return extId;
+    }
+
+    public void setExtId(Long extId) {
+        this.extId = extId;
+    }
+
+    public String getToolCode() {
+        return toolCode;
+    }
+
+    public void setToolCode(String toolCode) {
+        this.toolCode = toolCode;
+    }
+
+    public String getToolName() {
+        return toolName;
+    }
+
+    public void setToolName(String toolName) {
+        this.toolName = toolName;
+    }
+
+    public String getToolType() {
+        return toolType;
+    }
+
+    public void setToolType(String toolType) {
+        this.toolType = toolType;
+    }
+
+    public String getResState() {
+        return resState;
+    }
+
+    public void setResState(String resState) {
+        this.resState = resState;
+    }
+
+    public String getSelState() {
+        return selState;
+    }
+
+    public void setSelState(String selState) {
+        this.selState = selState;
+    }
+}
