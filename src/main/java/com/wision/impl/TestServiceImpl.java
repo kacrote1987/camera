620805642700d50d6af3af625e48b17217c4daa1
kacrote1987 <751613339@qq.com>
2024-11-02 12:26:21 +0800

    Update README.md

--- src/main/java/com/wision/impl/TestServiceImpl.java
+++ src/main/java/com/wision/impl/TestServiceImpl.java
@@ -1,19 +1,17 @@
 package com.wision.impl;
 
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.MatrixToImageConfig;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
+import com.wision.service.TestService;
+import com.wision.util.QRCodeGenerator;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Service;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Paths;
+@Service
+public class TestServiceImpl implements TestService {
+//    @Mapper
+//    QRCodeGenerator qrCodeGenerator;
 
-public class TestServiceImpl {
-
+    @Override
+    public void qrcode() {
+//        qrCodeGenerator.simple();
+    }
 }
