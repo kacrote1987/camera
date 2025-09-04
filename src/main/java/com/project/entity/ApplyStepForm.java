71e5d27669af0d027c2f849e599aaaa81ada545c
Administrator <751613339@qq.com>
2025-09-04 16:56:18 +0800

    ===

---
+++ src/main/java/com/project/entity/ApplyStepForm.java
@@ -0,0 +1,22 @@
+package com.project.entity;
+
+public class ApplyStepForm {
+    private Long applyId;
+    private String stepFurther;
+
+    public Long getApplyId() {
+        return applyId;
+    }
+
+    public void setApplyId(Long applyId) {
+        this.applyId = applyId;
+    }
+
+    public String getStepFurther() {
+        return stepFurther;
+    }
+
+    public void setStepFurther(String stepFurther) {
+        this.stepFurther = stepFurther;
+    }
+}
